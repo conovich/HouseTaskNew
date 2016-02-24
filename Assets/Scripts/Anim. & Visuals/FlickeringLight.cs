993ab692bb9b98d2616b56a06b61d6e081c9384f
conovich <conovich@gmail.com>
2016-02-24 12:12:28 -0500

    Baked lighting, tweaked other lighting, added fire lighting.

---
+++ Assets/Scripts/Anim. & Visuals/FlickeringLight.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//http://answers.unity3d.com/questions/742466/camp-fire-light-flicker-control.html
+
+public class FlickeringLight : MonoBehaviour {
+
+	float minFlickerIntensity = 2.0f;
+	float maxFlickerIntensity = 3.5f;
+	float flickerSpeed = 0.035f;
+	
+	private float randomizer = 0.0f;
+
+	Light light;
+
+	void Start(){
+		light = GetComponent<Light>();
+		StartCoroutine(Flicker());
+	}
+
+
+	IEnumerator Flicker(){
+		while (true){
+			if (randomizer == 0) {
+				light.intensity = (Random.Range (minFlickerIntensity, maxFlickerIntensity));
+				
+			}
+			else light.intensity = (Random.Range (minFlickerIntensity, maxFlickerIntensity));
+			
+			randomizer = Random.Range (0.0f, 1.1f);
+			yield return new WaitForSeconds (flickerSpeed);
+		}
+	}
+}
